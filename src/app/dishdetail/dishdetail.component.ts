7905e817223fe49c06648b88810980002d130270
rohithkumar001 <63514039+rohithkumar001@users.noreply.github.com>
2020-05-10 23:43:53 +0530

    Animations Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,24 +7,20 @@ import { switchMap } from 'rxjs/operators';
 import { MatSliderModule } from '@angular/material/slider';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(),
+    flyInOut(),
+    expand()
   ]
 })
 export class DishdetailComponent implements OnInit {
@@ -38,7 +34,7 @@ export class DishdetailComponent implements OnInit {
   comment: Comment;
   errMess: string;
   dishcopy: Dish;
-  visibility: 'shown';
+  visibility: 'hidden';
 
   formErrors = {
     'author': '',
